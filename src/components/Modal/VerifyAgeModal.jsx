a00912050ee3127933d6500f3200feae5843f314
devjorgeluis <jorgencnc0608@gmail.com>
2026-05-20 13:18:45 -0400

    verify age modal

--- src/components/Modal/VerifyAgeModal.jsx
+++ src/components/Modal/VerifyAgeModal.jsx
@@ -3,24 +3,34 @@ const VerifyAgeModal = ({ isOpen, onConfirm }) => {
     if (!isOpen) return null;
 
     return (
-        <>
-            <div className="modal" style={{ display: isOpen ? "block" : "none" }}>
-                <div className="modal__content-container">
-                    <div className="age-verification-desktop">
-                        <h2 className="age-verification-desktop__title">Verificación de Edad</h2>
-                        <span className="age-verification-desktop__sub-title">¿Eres mayor de <span>18+</span> años?</span>
-                        <div className="age-verification-desktop__button-group">
-                            <div className="age-verification-desktop__button-container">
-                                <button type="button" className="button-desktop button-desktop_color_default" onClick={() => onConfirm()}>Si</button>
-                            </div>
-                            <div className="age-verification-desktop__button-container">
-                                <a type="button" className="button-desktop button-desktop_color_purple-bordered" href="https://google.com">No</a>
-                            </div>
+        <app-modal className="dark">
+            <div className="modal is-visible">
+                <div className="modal-box">
+                    <h3>Este sitio está prohibido para menores de edad.</h3>
+                    <p>Declaro ser mayor de 18 años de edad.</p>
+                    <div className="button-container">
+                        <div className="custom-button d-block" style={{ width: "100%" }}>
+                            <button
+                                type="button"
+                                className="btn green btn-block btn-regular"
+                                onClick={() => onConfirm()}
+                            >
+                                <span>Si</span>
+                            </button>
+                        </div>
+                        <div className="custom-button d-block" style={{ width: "100%" }}>
+                            <a
+                                type="button"
+                                className="btn purple btn-block btn-regular"
+                                href="https://google.com"
+                            >
+                                <span>No</span>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </app-modal>
     );
 };
 
